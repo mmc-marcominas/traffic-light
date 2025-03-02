a241f1780dbcd63bb39f3faf0f3e1802746c166f
mmc-marcominas <mmc.marco@gmail.com>
2025-03-02 14:19:51 -0300

    feat(v2.2) bonus - allow v2 specify initial state

--- src/core/CommandLineHandler.cs
+++ src/core/CommandLineHandler.cs
@@ -12,23 +12,30 @@ public static class CommandLineHandler {
         }
 
         var version = options.GetKey("--version", new string[] { "v1", "v2" });
-        await RunSelectedVersion(version, cts);
+        var validStates = new string[] { "vehicle", "pedestrian", "intermitent", "" };
+        var state = options.GetKey("--state", validStates);
+        await RunSelectedVersion(version, state, cts);
     }
 
     private static void ShowHelper() {
-        Console.WriteLine($"{Environment.NewLine}Usage: {CommandlineName} [options]");
-        Console.WriteLine($"{Environment.NewLine}Options:");
+        var newLine = Environment.NewLine;
+        Console.WriteLine($"{newLine}Usage: {CommandlineName} [options]");
+        Console.WriteLine($"{newLine}Options:");
         Console.WriteLine("  --help                             Display this help message");
         Console.WriteLine("  --version=versionNumber            Required: specify implementation version");
-        Console.WriteLine($"{Environment.NewLine}Example:");
-        Console.WriteLine($"  {CommandlineName} --version=v1 {Environment.NewLine}");
-        Console.WriteLine($"Available versions:");
-        Console.WriteLine($" v1 - simple dictionary implementation");
-        Console.WriteLine($" v2 - vehicle, pedestrian and intermitent implementation with enum");
+        Console.WriteLine("  --state=stateType                  Optional: specify traffic light state");
+        Console.WriteLine($"{newLine}Examples:");
+        Console.WriteLine($"  {CommandlineName} --version=v1");
+        Console.WriteLine($"  {CommandlineName} --version=v2 --state=vehicle");
+        Console.WriteLine($"{newLine}Available versions:");
+        Console.WriteLine($"  v1 - simple dictionary implementation");
+        Console.WriteLine($"  v2 - vehicle, pedestrian and intermitent implementation with enum");
+        Console.WriteLine($"{newLine}Available states:");
+        Console.WriteLine($"  vehicle, pedestrian or intermitent {newLine}");
         Environment.Exit(0);
     }
 
-    private static async Task RunSelectedVersion(string version, CancellationTokenSource cts) {
+    private static async Task RunSelectedVersion(string version, string state, CancellationTokenSource cts) {
         Dictionary<string, IRunner> options = new() {
             {"v1", new DictionaryVersionRunner()},
             {"v2", new EnumVersionRunner()},
@@ -37,6 +44,11 @@ public static class CommandLineHandler {
         if (!options.TryGetValue(version, out var result)) {
             Console.WriteLine($"Version not found");
             Environment.Exit(1);
+        }
+
+        if (!string.IsNullOrEmpty(state)) {
+            Console.WriteLine($"State {state} received");
+            result.SetInitialState(state, cts);
         }
 
         await result.Run(cts);
